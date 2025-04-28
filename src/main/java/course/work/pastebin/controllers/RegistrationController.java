61b15bc86da60859afed29377ea6293fa59add4e
Artem Volk <artem_volk04@mail.ru>
2025-04-28 16:32:19 +0500

    Сделал html более грамотными

--- src/main/java/course/work/pastebin/controllers/RegistrationController.java
+++ src/main/java/course/work/pastebin/controllers/RegistrationController.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -21,7 +20,7 @@ public class RegistrationController {
     @Autowired
     public RegistrationController(
             UserDetailServiceImpl userDetailServiceImpl,
-            PasswordEncoder passwordEncoder // Добавьте параметр
+            PasswordEncoder passwordEncoder
     ) {
         this.userDetailServiceImpl = userDetailServiceImpl;
         this.passwordEncoder = passwordEncoder;
